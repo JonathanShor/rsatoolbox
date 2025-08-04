ce32486a0f729a53908351637129f329113973db
Jonathan Shor <jds814@nyu.edu>
2025-08-04 12:09:17 -0400

    Autoformatting: blackify and sort imports

--- src/rsatoolbox/util/searchlight.py
+++ src/rsatoolbox/util/searchlight.py
@@ -7,12 +7,12 @@ https://github.com/machow/pysearchlight
 @author: Daniel Lindh
 """
 import numpy as np
-from scipy.spatial.distance import cdist
-from tqdm import tqdm
 from joblib import Parallel, delayed
 from rsatoolbox.data.dataset import Dataset
-from rsatoolbox.rdm.calc import calc_rdm
 from rsatoolbox.rdm import RDMs
+from rsatoolbox.rdm.calc import calc_rdm
+from scipy.spatial.distance import cdist
+from tqdm import tqdm
 
 
 def _get_searchlight_neighbors(mask, center, radius=3):
@@ -42,7 +42,7 @@ def _get_searchlight_neighbors(mask, center, radius=3):
     # Generate grid of points
     X, Y, Z = np.meshgrid(x, y, z)
     data = np.vstack((X.ravel(), Y.ravel(), Z.ravel())).T
-    distance = cdist(data, center.reshape(1, -1), 'euclidean').ravel()
+    distance = cdist(data, center.reshape(1, -1), "euclidean").ravel()
 
     return tuple(data[distance < radius].T.tolist())
 
@@ -81,16 +81,17 @@ def get_volume_searchlight(mask, radius=2, threshold=1.0):
     good_centers = []
     good_neighbors = []
 
-    for center in tqdm(centers, desc='Finding searchlights...'):
+    for center in tqdm(centers, desc="Finding searchlights..."):
         neighbors = _get_searchlight_neighbors(mask, center, radius)
         if mask[neighbors].mean() >= threshold:
             good_centers.append(center)
             good_neighbors.append(neighbors)
 
     good_centers = np.array(good_centers)
-    assert good_centers.shape[0] == len(good_neighbors),\
-        "number of centers and sets of neighbors do not match"
-    print(f'Found {len(good_neighbors)} searchlights')
+    assert good_centers.shape[0] == len(
+        good_neighbors
+    ), "number of centers and sets of neighbors do not match"
+    print(f"Found {len(good_neighbors)} searchlights")
 
     # turn the 3-dim coordinates to array coordinates
     centers = np.ravel_multi_index(good_centers.T, mask.shape)
@@ -99,8 +100,7 @@ def get_volume_searchlight(mask, radius=2, threshold=1.0):
     return centers, neighbors
 
 
-def get_searchlight_RDMs(data_2d, centers, neighbors, events,
-                         method='correlation', verbose=True):
+def get_searchlight_RDMs(data_2d, centers, neighbors, events, method="correlation", verbose=True):
     """Iterates over all the searchlight centers and calculates the RDM
 
     Args:
@@ -134,28 +134,29 @@ def get_searchlight_RDMs(data_2d, centers, neighbors, events,
     if n_centers > 1000:
         # we can't run all centers at once, that will take too much memory
         # so lets to some chunking
-        chunked_center = np.split(np.arange(n_centers),
-                                  np.linspace(0, n_centers,
-                                              101, dtype=int)[1:-1])
+        chunked_center = np.split(
+            np.arange(n_centers), np.linspace(0, n_centers, 101, dtype=int)[1:-1]
+        )
 
         # loop over chunks
         n_conds = len(np.unique(events))
         RDM = np.zeros((n_centers, n_conds * (n_conds - 1) // 2))
-        for chunks in tqdm(chunked_center, desc='Calculating RDMs...'):
+        for chunks in tqdm(chunked_center, desc="Calculating RDMs..."):
             center_data = []
             for c in chunks:
                 # grab this center and neighbors
                 center = centers[c]
                 center_neighbors = neighbors[c]
                 # create a database object with this data
-                ds = Dataset(data_2d[:, center_neighbors],
-                             descriptors={'center': center},
-                             obs_descriptors={'events': events},
-                             channel_descriptors={'voxels': center_neighbors})
+                ds = Dataset(
+                    data_2d[:, center_neighbors],
+                    descriptors={"center": center},
+                    obs_descriptors={"events": events},
+                    channel_descriptors={"voxels": center_neighbors},
+                )
                 center_data.append(ds)
 
-            RDM_corr = calc_rdm(center_data, method=method,
-                                descriptor='events')
+            RDM_corr = calc_rdm(center_data, method=method, descriptor="events")
             RDM[chunks, :] = RDM_corr.dissimilarities
     else:
         center_data = []
@@ -164,23 +165,24 @@ def get_searchlight_RDMs(data_2d, centers, neighbors, events,
             center = centers[c]
             nb = neighbors[c]
             # create a database object with this data
-            ds = Dataset(data_2d[:, nb],
-                         descriptors={'center': c},
-                         obs_descriptors={'events': events},
-                         channel_descriptors={'voxels': nb})
+            ds = Dataset(
+                data_2d[:, nb],
+                descriptors={"center": c},
+                obs_descriptors={"events": events},
+                channel_descriptors={"voxels": nb},
+            )
             center_data.append(ds)
         # calculate RDMs for each database object
-        RDM = calc_rdm(center_data, method=method,
-                       descriptor='events').dissimilarities
+        RDM = calc_rdm(center_data, method=method, descriptor="events").dissimilarities
 
-    SL_rdms = RDMs(RDM,
-                   rdm_descriptors={'voxel_index': centers},
-                   dissimilarity_measure=method)
+    SL_rdms = RDMs(RDM, rdm_descriptors={"voxel_index": centers}, dissimilarity_measure=method)
 
     return SL_rdms
 
 
-def evaluate_models_searchlight(sl_RDM, models, eval_function, method='corr', theta=None, n_jobs=1):
+def evaluate_models_searchlight(
+    sl_RDM, models, eval_function, method="corr", theta=None, n_jobs=1
+):
     """evaluates each searchlighth with the given model/models
 
     Args:
@@ -202,8 +204,8 @@ def evaluate_models_searchlight(sl_RDM, models, eval_function, method='corr', th
     """
 
     results = Parallel(n_jobs=n_jobs)(
-        delayed(eval_function)(
-            models, x, method=method, theta=theta) for x in tqdm(
-            sl_RDM, desc='Evaluating models for each searchlight'))
+        delayed(eval_function)(models, x, method=method, theta=theta)
+        for x in tqdm(sl_RDM, desc="Evaluating models for each searchlight")
+    )
 
     return results
